5aeb37aadd68d2b65b1225bf42aa514074e3c599
Bill Barnes <wbarnes@threecolts.com>
2024-09-30 08:42:42 -0400

    Updates
    1 - bug fixes
    2 - new function, display_the_promo_code_fieldf

--- TA_Payment_Flow/tests/signup.py
+++ TA_Payment_Flow/tests/signup.py
@@ -4,30 +4,48 @@ from playwright.sync_api import sync_playwright, expect
 from dotenv import load_dotenv
 load_dotenv()
 
-from TA_Payment_Flow.tests.common_functions import unique_credentials, display_initial_page
+from common_functions import unique_credentials, display_initial_page
+
+def display_the_promo_code_field(page):
+    # Displays the promo code field and finally, ensure it is empty
+    try:
+        # Try to find the link with the first text option
+        link = page.locator("text='Enter a promo code'")
+        expect(link).to_be_visible()  # Check if the element is visible
+        link.click()
+    except Exception as e:
+        # If the first text is not found, try the alternative text
+        try:
+            link = page.locator("text='Enter a different promo code'")
+            expect(link).to_be_visible()
+            link.click()
+        except Exception as e:
+            # Handle the case where neither link is found
+            print("Neither promo code link found.")
+    finally:
+        promo_code_field = page.locator("div", has_text=re.compile(r"^ApplyPromo code$")).locator("input[placeholder=' ']")
+        promo_code_field.fill("")
 
 
 def enter_an_invalid_promo_code(page, promocode):
     # Enters an invalid code, verifies the error message, clicks the Cancel
     # button, verify the changed label text
-    page.get_by_text("Enter a promo code").click()
+    display_the_promo_code_field(page)
     page.locator("div").filter(has_text=re.compile(r"^ApplyPromo code$")).get_by_placeholder(" ").fill(promocode)
     page.locator("button").filter(has_text="Apply").click()
     expect(page.get_by_role("tooltip")).to_contain_text("Sorry, this code isn't valid")
     page.locator("button").filter(has_text="Cancel").click()
     expect(page.get_by_role("dialog")).to_contain_text("Enter a different promo code")
 
+
 def apply_20_percent_discount(page, promocode):
-    page.get_by_text("Enter a promo code").click()
+    display_the_promo_code_field(page)
     page.locator("button").filter(has_text="Apply").click()
 
 
 def ta_signup(pw1):
     # Convert the returned string to a boolean
     headless_mode = os.getenv("HEADLESS_MODE", "false").strip().lower() == "true"
-    # test it in all 3 browsers
-   # browser_types = ['chromium', 'firefox', 'webkit']
-    # for browser_type in browser_types:
     email, fullname, userpass = unique_credentials()
     page, browser, context, stage_manager_url = display_initial_page(pw1, os.getenv("BROWSER"), headless_mode, 'ta_reg',500)
     page.get_by_role("textbox").fill(email)
@@ -35,12 +53,16 @@ def ta_signup(pw1):
     page.locator("#app input[name=\"full_name\"]").fill(fullname)
     page.locator("input[name=\"password\"]").fill(userpass)
     page.locator("input[name=\"confirm_password\"]").fill(userpass)
+
     # Click the Continue button
     page.get_by_label("tc-button").click()
+
     # CLick on Reseller or Retailer
     page.get_by_text("Arbitrage", exact=True).click()
+
     # Click on Seller Central (Amazon)
     page.get_by_text("Youtube").click()
+
     # Now click the Finish button
     page.locator("button").filter(has_text="Finish").click()
     """
@@ -56,7 +78,9 @@ def ta_signup(pw1):
     #     "Card number").fill("4242 4242 4242 42422")
 
     # CLick on Enter a promo code
-    page.get_by_text("Enter a promo code").click()
+    # page.get_by_text("Enter a promo code").click()
+    display_the_promo_code_field(page)
+
     # Make sure the Cancel button works
     page.locator("button").filter(has_text="Cancel").click()
 
@@ -69,8 +93,13 @@ def ta_signup(pw1):
     J213BB9X for 20% off All plans
     OAC14DAY extends 14 trial - no discounts
     """
+
+    # Display the promo code field again
+    display_the_promo_code_field(page)
+
     # See if Monthly promo code works for yearly plan. It should fail
     page.locator("div").filter(has_text=re.compile(r"^ApplyPromo code$")).get_by_placeholder(" ").fill(os.getenv("FIFTY_PERCENT_OFF_MONTHLY_PLAN"))
+
     expect(page.get_by_role("dialog")).to_contain_text("This promo code is not valid for this plan.")
 
 
@@ -80,8 +109,9 @@ def ta_signup(pw1):
         expect(page.get_by_role("dialog")).to_contain_text("To activate your 7 day FREE trial")
     except AssertionError as e:
         print(f"An unexpected error occurred: {e}")
+
     # Enter cc Details
-    enter_cc_details(page)
+    # enter_cc_details(page)
 
 
 with sync_playwright() as pw:
